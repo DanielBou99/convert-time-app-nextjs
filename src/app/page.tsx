2ee3e968c2ac2770e944cb51a95136263fe0ba5a
Daniel Bou de Souza <danielbou@outlook.com.br>
2023-07-09 17:38:45 -0300

    Timezone list and refactor country

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
-import React, { useState } from "react"
+import React, { useState } from "react";
 import {
+  Container,
   FormControl,
   InputLabel,
   MenuItem,
@@ -9,99 +10,89 @@ import {
 } from "@mui/material";
 import { TimePicker } from "@mui/x-date-pickers";
 import dayjs from "dayjs";
+import timezone from "dayjs/plugin/timezone";
+import utc from "dayjs/plugin/utc";
+import timezones from 'timezones-list';
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
 export default function Home() {
-  const countries = [
-    {
-      id: 1,
-      label: "Brazil",
-    },
-    {
-      id: 2,
-      label: "Peru",
-    },
-  ];
-  const [selectedFirstCountry, setSelectedFirstCountry] = useState(
-    countries[0].id
+
+  const [selectedFirstTimeZone, setSelectedFirstTimeZone] = useState(
+    timezones[0].tzCode
   );
-  const [selectedSecondCountry, setSelectedSecondCountry] = useState(
-    countries[1].id
+  const [selectedSecondTimeZone, setSelectedSecondTimeZone] = useState(
+    timezones[1].tzCode
   );
   const [resultTime, setResultTime] = useState("Test");
 
-  const selectedCountryOneChange = (event: any) => {
-    const countrySelected = findCountryById(event.target.value);
-    if (countrySelected) setSelectedFirstCountry(countrySelected.id);
+  const selectedTimeZoneOneChange = (event: any) => {
+    setSelectedFirstTimeZone(event.target.value);
   };
 
-  const selectedCountryTwoChange = (event: any) => {
-    const countrySelected = findCountryById(event.target.value);
-    if (countrySelected) setSelectedSecondCountry(countrySelected.id);
-  };
-
-  const findCountryById = (id: number) => {
-    return countries.find((country) => country.id === id);
+  const selectedTimeZoneTwoChange = (event: any) => {
+    setSelectedSecondTimeZone(event.target.value)
   };
 
   const timePickerChanged = (event: any) => {
-    console.log(event);
+    console.log(dayjs.tz.guess());
   };
 
   return (
-    <section>
-      {selectedFirstCountry}
+    <Container maxWidth="sm">
       <h1>Convert Time</h1>
       <div>
         <div>
-          <p>Country 1</p>
+          <p>Time Zone 1</p>
           <FormControl fullWidth>
-            <InputLabel id="select-country-one">Country 1</InputLabel>
+            <InputLabel id="select-timezone-one">Time Zone 1</InputLabel>
             <Select
-              labelId="select-country-one"
-              id="select-country-one"
-              label="Country 1"
-              onChange={selectedCountryOneChange}
-              value={selectedFirstCountry}
+              labelId="select-timezone-one"
+              id="select-timezone-one"
+              label="Time Zone 1"
+              onChange={selectedTimeZoneOneChange}
+              value={selectedFirstTimeZone}
             >
-              {countries.map((country) => (
-                <MenuItem key={country.id} value={country.id}>
-                  {country.label}
+              {timezones.map((tz) => (
+                <MenuItem key={tz.tzCode} value={tz.tzCode}>
+                  {tz.tzCode}
                 </MenuItem>
               ))}
             </Select>
           </FormControl>
         </div>
         <div>
-          <p>Time in country 1</p>
+          <p>Time in Time Zone 1</p>
           <TimePicker
             label="Uncontrolled picker"
             onChange={timePickerChanged}
-            defaultValue={dayjs("2022-04-17T15:30")}
+            defaultValue={dayjs(new Date())}
           />
         </div>
       </div>
       <div>
         <div>
-          <p>Country 2</p>
+          <p>Time Zone 2</p>
           <FormControl fullWidth>
-            <InputLabel id="select-country-two">Country 2</InputLabel>
+            <InputLabel id="select-timezone-two">Time Zone 2</InputLabel>
             <Select
-              labelId="select-country-two"
-              id="select-country-two"
-              label="Country 2"
-              onChange={selectedCountryTwoChange}
-              value={selectedSecondCountry}
+              labelId="select-timezone-two"
+              id="select-timezone-two"
+              label="Time Zone 2"
+              onChange={selectedTimeZoneTwoChange}
+              value={selectedSecondTimeZone}
             >
-              {countries.map((country) => (
-                <MenuItem key={country.id} value={country.id}>
-                  {country.label}
+              {timezones.map((tz) => (
+                <MenuItem key={tz.tzCode} value={tz.tzCode}>
+                  {tz.tzCode}
                 </MenuItem>
               ))}
             </Select>
           </FormControl>
         </div>
         <div>
-          <p>Time in country 2</p>
+          <p>Time in Time Zone s2</p>
           <TextField
             disabled
             id="outlined-disabled"
@@ -110,6 +101,6 @@ export default function Home() {
           />
         </div>
       </div>
-    </section>
+    </Container>
   );
 }
